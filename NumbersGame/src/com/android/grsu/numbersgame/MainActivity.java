69b5f043f76e77856a84f691e8b2acf74dd4c480
alex dzeshko <a.dzeshko@gmail.com>
2013-07-01 17:01:33 +0300

    init

--- NumbersGame/src/com/android/grsu/numbersgame/MainActivity.java
+++ NumbersGame/src/com/android/grsu/numbersgame/MainActivity.java
@@ -1,15 +1,39 @@
 package com.android.grsu.numbersgame;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+import com.android.grsu.numbersgame.fragment.GameFragment;
+
+public class MainActivity extends FragmentActivity {
+
+	private String[] mModeTitles;
+	private ListView mDrawerList;
+	private CharSequence mTitle;
+	private DrawerLayout mDrawerLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mModeTitles = getResources().getStringArray(R.array.modes_array);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		// Set the adapter for the list view
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, mModeTitles));
+		// Set the list's click listener
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 	}
 
 	@Override
@@ -19,4 +43,39 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	private class DrawerItemClickListener implements
+			ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView parent, View view, int position,
+				long id) {
+			selectItem(position);
+		}
+	}
+
+	/** Swaps fragments in the main content view */
+	private void selectItem(int position) {
+		// Create a new fragment and specify the planet to show based on
+		// position
+		Fragment fragment = new GameFragment();
+		Bundle args = new Bundle();
+		args.putInt(GameFragment.MODE_NUMBER, position);
+		fragment.setArguments(args);
+
+		// Insert the fragment by replacing any existing fragment
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		fragmentManager.beginTransaction()
+				.replace(R.id.content_frame, fragment).commit();
+
+		// Highlight the selected item, update the title, and close the drawer
+		mDrawerList.setItemChecked(position, true);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+			setTitle(mModeTitles[position]);
+		mDrawerLayout.closeDrawer(mDrawerList);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
+	}
 }
---
+++ NumbersGame/src/com/android/grsu/numbersgame/fragment/GameFragment.java
@@ -0,0 +1,63 @@
+package com.android.grsu.numbersgame.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.android.grsu.numbersgame.R;
+
+public class GameFragment extends Fragment implements OnClickListener {
+
+	public static final String MODE_NUMBER = "MODE_NUM";
+	
+	private static final int BUTTON_1 = R.id.button1;
+	private static final int BUTTON_2 = R.id.button2;
+	private static final int BUTTON_3 = R.id.button3;
+	private static final int BUTTON_4 = R.id.button4;
+	private static final int BUTTON_5 = R.id.button5;
+	private static final int BUTTON_6 = R.id.button6;
+	private int mMode;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mMode = getArguments().getInt(MODE_NUMBER);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_game, null);
+		Button button1 = (Button) view.findViewById(BUTTON_1);
+		Button button2 = (Button) view.findViewById(BUTTON_2);
+		Button button3 = (Button) view.findViewById(BUTTON_3);
+		Button button4 = (Button) view.findViewById(BUTTON_4);
+		Button button5 = (Button) view.findViewById(BUTTON_5);
+		Button button6 = (Button) view.findViewById(BUTTON_6);
+		button1.setOnClickListener(this);
+		button2.setOnClickListener(this);
+		button3.setOnClickListener(this);
+		button4.setOnClickListener(this);
+		button5.setOnClickListener(this);
+		button6.setOnClickListener(this);
+		return view;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case BUTTON_1:
+			
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+
+}
