d888ea43f45e2cb7d33281d90e7dd093abe4e81f
alex dzeshko <a.dzeshko@gmail.com>
2013-07-05 14:59:51 +0300

    random

--- NumbersGame/src/com/android/grsu/numbersgame/fragment/GameFragment.java
+++ NumbersGame/src/com/android/grsu/numbersgame/fragment/GameFragment.java
@@ -1,11 +1,16 @@
 package com.android.grsu.numbersgame.fragment;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -26,6 +31,7 @@ public class GameFragment extends Fragment implements OnClickListener,
 		OnFinishListener {
 
 	public static final String MODE_NUMBER = "MODE_NUM";
+	public static final String MODE = "MODE";
 
 	private int mMode;
 	private TextView mTextViewTask, mTextViewResult;
@@ -33,19 +39,33 @@ public class GameFragment extends Fragment implements OnClickListener,
 	private ActivityCallback mActivityCallback;
 	// private OnFinishListener mFinishCallback;
 	private SoundManager mSoundManager;
+	private boolean mHardMode;
+	private Button[] mButtons = new Button[10];
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		mMode = getArguments().getInt(MODE_NUMBER);
-		// mFinishCallback = new OnFinishListener() {
-		//
-		// @Override
-		// public void finish() {
-		//
-		// }
-		// };
+		mHardMode = getArguments().getBoolean(MODE);
+		
+	}
 
+	private void randomize() {
+		if(mHardMode){
+			Random r = new Random();
+			for(int i = 0; i<mButtons.length;i++){
+				Button buttonA = mButtons[r.nextInt(9)];
+				int a = (Integer) buttonA.getTag();
+				Drawable dra = buttonA.getBackground();
+				
+				buttonA.setTag(mButtons[i].getTag());
+				buttonA.setBackgroundDrawable(mButtons[i].getBackground());
+				mButtons[i].setTag(a);
+				mButtons[i].setBackgroundDrawable(dra);
+			}
+		}
+		
 	}
 
 	private void initMode() {
@@ -70,6 +90,7 @@ public class GameFragment extends Fragment implements OnClickListener,
 			// mGameManager = ComeOnGues.getInstance(getActivity(),
 			// mTextViewTask,
 			// mTextViewResult, imageView, mFinishCallback);
+			break;
 		default:
 			break;
 		}
@@ -83,37 +104,22 @@ public class GameFragment extends Fragment implements OnClickListener,
 		View view = inflater.inflate(R.layout.fragment_game, null);
 		mTextViewTask = (TextView) view.findViewById(R.id.text_view_task);
 		mTextViewResult = (TextView) view.findViewById(R.id.text_view_results);
-		Button button1 = (Button) view.findViewById(R.id.button1);
-		Button button2 = (Button) view.findViewById(R.id.button2);
-		Button button3 = (Button) view.findViewById(R.id.button3);
-		Button button4 = (Button) view.findViewById(R.id.button4);
-		Button button5 = (Button) view.findViewById(R.id.button5);
-		Button button6 = (Button) view.findViewById(R.id.button6);
-		Button button7 = (Button) view.findViewById(R.id.button7);
-		Button button8 = (Button) view.findViewById(R.id.button8);
-		Button button9 = (Button) view.findViewById(R.id.button9);
-		Button button0 = (Button) view.findViewById(R.id.button0);
-		button1.setOnClickListener(this);
-		button2.setOnClickListener(this);
-		button3.setOnClickListener(this);
-		button4.setOnClickListener(this);
-		button5.setOnClickListener(this);
-		button6.setOnClickListener(this);
-		button7.setOnClickListener(this);
-		button8.setOnClickListener(this);
-		button9.setOnClickListener(this);
-		button0.setOnClickListener(this);
-
-		button0.setTag(0);
-		button1.setTag(1);
-		button2.setTag(2);
-		button3.setTag(3);
-		button4.setTag(4);
-		button5.setTag(5);
-		button6.setTag(6);
-		button7.setTag(7);
-		button8.setTag(8);
-		button9.setTag(9);
+		mButtons[1] = (Button) view.findViewById(R.id.button1);
+		mButtons[2] = (Button) view.findViewById(R.id.button2);
+		mButtons[3] = (Button) view.findViewById(R.id.button3);
+		mButtons[4] = (Button) view.findViewById(R.id.button4);
+		mButtons[5] = (Button) view.findViewById(R.id.button5);
+		mButtons[6] = (Button) view.findViewById(R.id.button6);
+		mButtons[7] = (Button) view.findViewById(R.id.button7);
+		mButtons[8] = (Button) view.findViewById(R.id.button8);
+		mButtons[9] = (Button) view.findViewById(R.id.button9);
+		mButtons[0] = (Button) view.findViewById(R.id.button0);
+		for(int i = 0; i<mButtons.length; i++){
+			mButtons[i].setOnClickListener(this);
+			mButtons[i].setTag(i);
+		}
+			
+		randomize();
 
 		initMode();
 
@@ -123,6 +129,13 @@ public class GameFragment extends Fragment implements OnClickListener,
 	@Override
 	public void onClick(View v) {
 		mGameManager.buttonPressed((Integer) v.getTag());
+		randomize();
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.main, menu);
+
 	}
 
 	@Override
